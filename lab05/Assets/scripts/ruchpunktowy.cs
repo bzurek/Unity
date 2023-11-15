a87f30aaf6d026b20eb54e4dd05dfcf8b93293b4
Barbara Zurek <barbara.zurek999@gmail.com>
2023-11-15 01:05:53 +0100

    dodanie zadan 1-2 z lab 5

---
+++ lab05/Assets/scripts/ruchpunktowy.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ruchpunktowy : MonoBehaviour
+{
+    public List<Vector3> punkty;
+    private Vector3 start;
+    public float platspeed = 3.1f;
+    private bool czyprzod = true;
+    private bool czytyl = false;
+    private bool czyruch = true;
+    private bool czypunkt = false;
+    private Vector3 direction;
+    private Transform oldParent;
+
+
+    void Start()
+    {
+        start = transform.position;
+    }
+
+//    void FixedUpdate()
+//    {
+//        if (isRunningForward && transform.position.x)  >= czypunkt.x )
+//        {
+//            czyruch = false;
+//        }
+//        else if (isRunningBackward && transform.position.x <= startPosition.x)
+//        {
+//            isRunning = false;
+//        }
+
+//        if (czyruch)
+//        {
+
+//            if (isRunningForward) direction = finalPosition;
+//            else if (isRunningBackward) direction = -finalPosition;
+
+//            Vector3 move = direction.normalized * platspeed * Time.deltaTime;
+//            transform.Translate(move);
+//        }
+//    }
+
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.gameObject.CompareTag("Player"))
+//        {
+//            Debug.Log("Player wszedł na windę.");
+//            oldParent = other.gameObject.transform.parent;
+//            Debug.Log(oldParent);
+//            other.gameObject.transform.parent = transform;
+
+//            if (transform.position.x >= finalPosition.x)
+//            {
+//                isRunningBackward = true;
+//                isRunningForward = false;
+
+//            }
+//            else if (transform.position.x <= startPosition.x)
+//            {
+//                isRunningBackward = false;
+//                isRunningForward = true;
+
+//            }
+//            isRunning = true;
+//        }
+//    }
+
+//    private void OnTriggerExit(Collider other)
+//    {
+//        if (other.gameObject.CompareTag("Player"))
+//        {
+//            Debug.Log("Player zszedł z windy.");
+//            other.gameObject.transform.parent = oldParent;
+//        }
+//    }
+}
---
+++ lab05/Assets/scripts/testmove.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlatformMove : MonoBehaviour
+{
+    public Vector3 startPosition;
+    public Vector3 finalPosition;
+    public float xDistance = 5;
+    public float zDistance = 5;
+    public float elevatorSpeed = 2f;
+    private bool isRunning = false;
+    private bool isRunningForward = true;
+    private bool isRunningBackward = false;
+    private Vector3 direction;
+    private Transform oldParent;
+
+
+    void Start()
+    {
+        startPosition = transform.position;
+        finalPosition = new Vector3(transform.position.x + xDistance, transform.position.y, transform.position.z + zDistance);
+    }
+
+    void FixedUpdate()
+    {
+        if (isRunningForward && transform.position.x >= finalPosition.x)
+        {
+            isRunning = false;
+        }
+        else if (isRunningBackward && transform.position.x <= startPosition.x)
+        {
+            isRunning = false;
+        }
+
+        if (isRunning)
+        {
+
+            if (isRunningForward) direction = finalPosition;
+            else if (isRunningBackward) direction = -finalPosition;
+            //else direction = startPosition;
+
+            Vector3 move = direction.normalized * elevatorSpeed * Time.deltaTime;
+            transform.Translate(move);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player wszedł na windę.");
+            oldParent = other.gameObject.transform.parent;
+            Debug.Log(oldParent);
+            other.gameObject.transform.parent = transform;
+
+            if (transform.position.x >= finalPosition.x)
+            {
+                isRunningBackward = true;
+                isRunningForward = false;
+
+            }
+            else if (transform.position.x <= startPosition.x)
+            {
+                isRunningBackward = false;
+                isRunningForward = true;
+
+            }
+            isRunning = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player zszedł z windy.");
+            other.gameObject.transform.parent = oldParent;
+        }
+    }
+}
